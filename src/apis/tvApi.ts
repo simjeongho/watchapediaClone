23cf8041922fb15f4380ed0db4790d1461921861
simjeongho012@gmail.com <simjeongho012@gmail.com>
2022-02-19 11:39:28 +0900

    api: tv api 연결

---
+++ src/apis/tvApi.ts
@@ -0,0 +1,16 @@
+import axiosInstance from ".";
+export const latestApi = () => axiosInstance.get("/tv/latest");
+
+export const airingTodayApi = () => axiosInstance.get("/tv/airing_today");
+
+export const onTheAirApi = () => axiosInstance.get(`/tv/on_the_air`);
+
+export const popularTvApi = () => axiosInstance.get("/tv/popular");
+
+export const topRatedTvApi = () => axiosInstance.get("/tv/top_rated");
+
+export const detailTvApi = (tvId: string) => axiosInstance.get(`/tv/${tvId}`);
+
+export const similarTvApi = (tvId: string) => axiosInstance.get(`/tv/${tvId}/similar`);
+
+export const searchTvApi = (query: string) => axiosInstance.get(`/search/tv?/query=${query}`);
