e712d3db4d83f46664fabb5845e25ef9dd452362
simjeongho012@gmail.com <simjeongho012@gmail.com>
2022-02-25 16:32:10 +0900

    feat: 기본 세팅 정리

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<QueryClientProvider client={queryClient}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</QueryClientProvider>,
 	document.getElementById("root"),
 );
 
