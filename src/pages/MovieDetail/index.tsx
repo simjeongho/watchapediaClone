c954763f1d436552eefa154e09b88e577a164430
simjeongho012@gmail.com <simjeongho012@gmail.com>
2022-02-28 20:16:07 +0900

    design: movie detail page 마크업

--- src/pages/MovieDetail/index.tsx
+++ src/pages/MovieDetail/index.tsx
@@ -1,7 +1,82 @@
+import {
+	Backdrop,
+	MovieDetailAction,
+	MovieDetailActionButton,
+	MovieDetailActionButtonContainer,
+	MovieDetailAverageRate,
+	MovieDetailBackDropImage,
+	MovieDetailBottomInfo,
+	MovieDetailContainer,
+	MovieDetailContentSectionContainer,
+	MovieDetailDivider,
+	MovieDetailKeyWord,
+	MovieDetailLeftBlur,
+	MovieDetailLeftGradient,
+	MovieDetailMain,
+	MovieDetailPoster,
+	MovieDetailPosterContainer,
+	MovieDetailPosterWrapper,
+	MovieDetailRatingWrapper,
+	MovieDetailRightBlur,
+	MovieDetailRightGradient,
+	MovieDetailStarRate,
+	MovieDetailStarRateText,
+	MovieDetailTitle,
+	MovieDetailTopInfo,
+	DefaultInfo,
+	Similar,
+} from "./styled";
+
 const MovieDetailPage = () => {
 	return (
 		<>
-			<h1>movie detail</h1>
+			<MovieDetailTopInfo>
+				<MovieDetailPosterContainer>
+					<Backdrop>
+						<MovieDetailLeftBlur />
+						<MovieDetailBackDropImage>
+							<MovieDetailLeftGradient />
+							<MovieDetailRightGradient />
+						</MovieDetailBackDropImage>
+						<MovieDetailRightBlur />
+					</Backdrop>
+				</MovieDetailPosterContainer>
+
+				<MovieDetailMain>
+					<MovieDetailContainer>
+						<MovieDetailPosterWrapper>
+							<MovieDetailPoster />
+						</MovieDetailPosterWrapper>
+
+						<MovieDetailTitle></MovieDetailTitle>
+						<MovieDetailKeyWord></MovieDetailKeyWord>
+
+						<MovieDetailAverageRate></MovieDetailAverageRate>
+						<MovieDetailAction>
+							<MovieDetailStarRate>
+								<MovieDetailStarRateText></MovieDetailStarRateText>
+								<MovieDetailRatingWrapper></MovieDetailRatingWrapper>
+							</MovieDetailStarRate>
+
+							<MovieDetailDivider />
+
+							<MovieDetailActionButtonContainer>
+								<MovieDetailActionButton>보고싶어요</MovieDetailActionButton>
+								<MovieDetailActionButton>코멘트</MovieDetailActionButton>
+								<MovieDetailActionButton>보는 중</MovieDetailActionButton>
+								<MovieDetailActionButton>더보기</MovieDetailActionButton>
+							</MovieDetailActionButtonContainer>
+						</MovieDetailAction>
+					</MovieDetailContainer>
+				</MovieDetailMain>
+			</MovieDetailTopInfo>
+
+			<MovieDetailBottomInfo>
+				<MovieDetailContentSectionContainer>
+					<DefaultInfo />
+					<Similar />
+				</MovieDetailContentSectionContainer>
+			</MovieDetailBottomInfo>
 		</>
 	);
 };
